9b7b35350452be05fceb1b6348c56505259b205b
TayTufenkjian <taytuf@gmail.com>
2013-11-22 18:02:42 -0500

    smooth scroll and initial active nav styling

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
-    $(".menu").accordion({collapsible: true, active: false, heightStyle: "content"});
+//accordion menu for Blog
+    $(".menu").accordion({collapsible: true, active: false, heightStyle: "content"});   
+
+//smooth scroll for Projects
+    $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 });
 
 
 
+// jQuery test
 // $(document).ready(function(){
 // 	$("img").click(function(){
 // 		$(this).fadeOut("fast");
 // 	});
-// 	$(function)
 // });
