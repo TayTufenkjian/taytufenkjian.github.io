bfb2c0e89c2c5e9d902f9fc7204a9ae5dfd2fab1
TayTufenkjian <taytuf@gmail.com>
2013-11-27 15:53:59 -0500

    fadeIn pages

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
 $(document).ready(function() {
+  $("body").css("display", "none");
+  $("body").fadeIn(1000);
 //accordion menu for Blog
-    $(".menu").accordion({collapsible: true, active: false, heightStyle: "content"});   
-
-//smooth scroll for Projects
-    $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+  $(".menu").accordion({collapsible: true, active: false, heightStyle: "content"});   
 
 });
 
