250575d0fd2b7b059b52991b576be6b0adbe6fc9
TayTufenkjian <taytuf@gmail.com>
2013-12-08 22:22:50 -0500

    Nivo Slider is working

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
   $("body").css("display", "none");
   $("body").fadeIn(1000);
-//accordion menu for Blog
+
+//accordion menu for Thoughts page
   $(".menu").accordion({collapsible: true, active: false, heightStyle: "content"});   
+
+//Nivo Slider for Projects page
+$(window).load(function() {
+    $("#slider").nivoSlider();
+	});
 
 });
 
