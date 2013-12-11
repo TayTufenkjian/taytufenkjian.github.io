aae1906a8ba1441dc5f2ff98c46445f60b2aad05
TayTufenkjian <taytuf@gmail.com>
2013-12-11 09:11:01 -0500

    madlibs test page separate from Projects

--- js/script.js
+++ js/script.js
@@ -2,30 +2,44 @@ $(document).ready(function() {
   $("body").css("display", "none");
   $("body").fadeIn(1000);
   
-  // Form for Pitch Madlib
-  $("form").css("display", "none");
-  $("#startPitch").click(function(){
-  	$(this).fadeOut(500);
-  	$("form").fadeIn(500);
+  // Form for Pitch Madlib 
+  var pitchForm ='<form><p>Enter a noun. (You know, a person, place, or thing.) <br> <input type="text" id="firstNoun" name="firstNoun"/></p><p>Enter another noun.<br><input type="text" id="secondNoun" name="secondNoun"/></p> <p>Wonderful.  How about an adjective?<br><input type="text" id="adjective" name="adjective"/></p><p>Time for one more noun.<br><input type="text" id="thirdNoun" name="thirdNoun"/></p><p>Now clench it with a verb.<br><input type="text" id="verb" name="verb"/></p><input type="submit" name="submit" value="See your pitch"/></form>'
+
+   $("#startPitch").click(function(){
+    $(".content").html(pitchForm);
+    });
+
+  $("form").submit(function(e){
+  e.preventDefault();  
+  var firstNoun = ("#firstNoun").val();
+  var secondNoun= ("#secondNoun").val();
+  var adjective = ("#adjective").val();
+  var thirdNoun = ("#thirdNoun").val();
+  var verb = ("#verb").val();
+
+  alert("oh hai!");
+    });
+  
+  // Personal Pitch Madlib
+  // I'm a (firstNoun) looking for (secondNoun) at a (adjective) company 
+  // where I can use my skills in (thirdNoun) to (verb).
+
+  //Nivo Slider for Projects page
+  $(window).load(function() {
+      $("#slider").nivoSlider();
+    });
+
   });
-  // $("#submit").click(function(){
-  // 	var firstNoun = $("input:text[name=firstNoun"]).val();
-  // 	$("form").fadeOut(500);
-  // 	$("startPitch").append("I'm a " + firstNoun);
-  // });
+
+
+
+  
 
 //accordion menu for Thoughts page
   $(".menu").accordion({collapsible: true, active: false, heightStyle: "content"});   
 
-//Nivo Slider for Projects page
-$(window).load(function() {
-    $("#slider").nivoSlider();
-	});
-
-});
 
 
 
-// Personal Pitch Madlib
-// I'm a (noun) looking for (noun) at a (adjective) company 
-// where I can use my skills in (noun) to (verb).
+
+
