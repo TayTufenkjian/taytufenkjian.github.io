c2469044af37c569aa581375e43a62e6520e9d3a
TayTufenkjian <taytuf@gmail.com>
2013-12-10 22:47:55 -0500

    personal pitch madlibs first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
   $("body").css("display", "none");
   $("body").fadeIn(1000);
+  
+  // Form for Pitch Madlib
+  $("form").css("display", "none");
+  $("#startPitch").click(function(){
+  	$(this).fadeOut(500);
+  	$("form").fadeIn(500);
+  });
+  // $("#submit").click(function(){
+  // 	var firstNoun = $("input:text[name=firstNoun"]).val();
+  // 	$("form").fadeOut(500);
+  // 	$("startPitch").append("I'm a " + firstNoun);
+  // });
 
 //accordion menu for Thoughts page
   $(".menu").accordion({collapsible: true, active: false, heightStyle: "content"});   
@@ -13,3 +25,7 @@ $(window).load(function() {
 });
 
 
+
+// Personal Pitch Madlib
+// I'm a (noun) looking for (noun) at a (adjective) company 
+// where I can use my skills in (noun) to (verb).
