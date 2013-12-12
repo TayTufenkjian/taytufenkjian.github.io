1b61e3857df116e7586671c910c50e94ee22f3ba
TayTufenkjian <taytuf@gmail.com>
2013-12-11 22:11:19 -0500

    Google Analytics tracking added

--- js/script.js
+++ js/script.js
@@ -3,22 +3,26 @@ $(document).ready(function() {
   $("body").fadeIn(1000);
   
   // Form for Pitch Madlib 
-  var pitchForm ='<form><p>Enter a noun. (You know, a person, place, or thing.) <br> <input type="text" id="firstNoun" name="firstNoun"/></p><p>Enter another noun.<br><input type="text" id="secondNoun" name="secondNoun"/></p> <p>Wonderful.  How about an adjective?<br><input type="text" id="adjective" name="adjective"/></p><p>Time for one more noun.<br><input type="text" id="thirdNoun" name="thirdNoun"/></p><p>Now clench it with a verb.<br><input type="text" id="verb" name="verb"/></p><input type="submit" name="submit" value="See your pitch"/></form>'
+  var pitchForm ='<form><p>Enter a noun. (You know, a person, place, or thing.) <br> <input type="text" id="firstNoun" name="firstNoun"/></p><p>Enter another noun.<br><input type="text" id="secondNoun" name="secondNoun"/></p> <p>Wonderful.  How about an adjective?<br><input type="text" id="adjective" name="adjective"/></p><p>Time for one more noun.<br><input type="text" id="thirdNoun" name="thirdNoun"/></p><p>Now clench it with a verb.<br><input type="text" id="verb" name="verb"/></p><input type="submit" name="submit" value="Pitch it"/></form>'
 
    $("#startPitch").click(function(){
     $(".content").html(pitchForm);
+    $("form").submit(function(e){
+      e.preventDefault();    
+      var firstNoun = ("#firstNoun").val();
+      alert("oh hai");   
+      });
+
     });
 
-  $("form").submit(function(e){
-  e.preventDefault();  
-  var firstNoun = ("#firstNoun").val();
-  var secondNoun= ("#secondNoun").val();
-  var adjective = ("#adjective").val();
-  var thirdNoun = ("#thirdNoun").val();
-  var verb = ("#verb").val();
 
-  alert("oh hai!");
-    });
+
+  // var firstNoun = ("#firstNoun").val();
+  // var secondNoun= ("#secondNoun").val();
+  // var adjective = ("#adjective").val();
+  // var thirdNoun = ("#thirdNoun").val();
+  // var verb = ("#verb").val();
+ 
   
   // Personal Pitch Madlib
   // I'm a (firstNoun) looking for (secondNoun) at a (adjective) company 
