acab91ddf57020c41d4d9420a8ad3ef1411599d6
TayTufenkjian <taytuf@gmail.com>
2013-12-17 12:02:33 -0500

    Bookalokal project page updates - working Lightbox

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $("body").css("display", "none");
   $("body").fadeIn(1000);
+  //Nivo Lightbox for Bookalokal page under Projects
+  $('div.lightbox a').nivoLightbox();
   
   // Form for Pitch Madlib 
   var pitchForm ='<form><p>Enter a noun. (You know, a person, place, or thing.) <br> <input type="text" id="firstNoun" name="firstNoun"/></p><p>Enter another noun.<br><input type="text" id="secondNoun" name="secondNoun"/></p> <p>Wonderful.  How about an adjective?<br><input type="text" id="adjective" name="adjective"/></p><p>Time for one more noun.<br><input type="text" id="thirdNoun" name="thirdNoun"/></p><p>Now clench it with a verb.<br><input type="text" id="verb" name="verb"/></p><input type="submit" name="submit" value="Pitch it"/></form>'
@@ -36,8 +38,7 @@ $(document).ready(function() {
   });
 
 
-  //Nivo Lightbox for Bookalokal page under Projects
-  $('a').nivoLightbox();
+
 
 
 
