728000fa3c9ecef2894358691cfe835e22c522fe
TayTufenkjian <taytuf@gmail.com>
2013-12-18 13:25:14 -0500

    pitch madlib working in script tags

--- js/script.js
+++ js/script.js
@@ -1,35 +1,31 @@
-$(document).ready(function() {
-  $("body").css("display", "none");
-  $("body").fadeIn(1000);
+$(document).ready(function() { 
+  $('body').css('display', 'none');
+  $('body').fadeIn(1000);
   //Nivo Lightbox for Bookalokal page under Projects
   $('div.lightbox a').nivoLightbox();
   
-  // Form for Pitch Madlib 
-  var pitchForm ='<form><p>Enter a noun. (You know, a person, place, or thing.) <br> <input type="text" id="firstNoun" name="firstNoun"/></p><p>Enter another noun.<br><input type="text" id="secondNoun" name="secondNoun"/></p> <p>Wonderful.  How about an adjective?<br><input type="text" id="adjective" name="adjective"/></p><p>Time for one more noun.<br><input type="text" id="thirdNoun" name="thirdNoun"/></p><p>Now clench it with a verb.<br><input type="text" id="verb" name="verb"/></p><input type="submit" name="submit" value="Pitch it"/></form>'
-
-   $("#startPitch").click(function(){
-    $(".content").html(pitchForm);
-    $("form").submit(function(e){
-      e.preventDefault();    
-      var firstNoun = ("#firstNoun").val();
-      alert("oh hai");   
-      });
-
-    });
+  // $(function(){
+  //   var processPitch = function(form) {  
+  //   var firstNoun = form.firstNoun.value;
+  //   var secondNoun = form.secondNoun.value;
+  //   var adjective = form.adjective.value;
+  //   var thirdNoun = form.thirdNoun.value;
+  //   var verb = form.verb.value;
+  //   alert("I'm a " + firstNoun + " looking for " + secondNoun + " at a " + adjective + " company where I can use my skills in " + thirdNoun + " to " + verb + ".");
+  //   }
+  // });
 
 
+//the madlibs code that works in script tags
+  // function processPitch(form) {  
+  //   var firstNoun = form.firstNoun.value;
+  //   var secondNoun = form.secondNoun.value;
+  //   var adjective = form.adjective.value;
+  //   var thirdNoun = form.thirdNoun.value;
+  //   var verb = form.verb.value;
+  //   alert("I'm a " + firstNoun + " looking for " + secondNoun + " at a " + adjective + " company where I can use my skills in " + thirdNoun + " to " + verb + ".");
+  // }
 
-  // var firstNoun = ("#firstNoun").val();
-  // var secondNoun= ("#secondNoun").val();
-  // var adjective = ("#adjective").val();
-  // var thirdNoun = ("#thirdNoun").val();
-  // var verb = ("#verb").val();
- 
-  
-  // Personal Pitch Madlib
-  // I'm a (firstNoun) looking for (secondNoun) at a (adjective) company 
-  // where I can use my skills in (thirdNoun) to (verb).
- 
 
 //accordion menu for Thoughts page
   $(".menu").accordion({collapsible: true, active: false, heightStyle: "content"});   
