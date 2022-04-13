f133bae263d7c5a2b9d8636c3d52f0b9a33bedd6
TayTufenkjian <taytuf@gmail.com>
2022-04-13 16:53:40 -0400

    Delete old projects
    Removed demo projects that were several years old,
    some of which I don't even remember doing

--- ProductJavascript-master/application.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("body").css("display", "none");
-	$("body").fadeIn(1000);
-	$(".description").css("display", "none");
-	$(". product .details").click(function(){
-		$(".product .description").fadeIn(1000);
-	});
-});
