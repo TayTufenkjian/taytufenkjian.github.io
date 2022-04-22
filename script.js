171d46909aee5f0ad0f86df91f2c9edc20f6b198
TayTufenkjian <taytuf@gmail.com>
2022-04-22 13:22:58 -0400

    Toggle mobile nav menu when menu icon is clicked

--- script.js
+++ script.js
@@ -13,6 +13,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
         })
     })
+
+    // Hide mobile menu if window is resized to a large enough width
+    let mobileMenu = document.querySelector('#mobile-menu');
+    window.onresize = function () {
+        if (window.innerWidth > 670 && mobileMenu.style.display === 'block') {
+            mobileMenu.style.display = 'none';
+        }
+    }
 })
 
-
+function toggleNav() {
+    let mobileMenu = document.querySelector('#mobile-menu');
+    if (mobileMenu.style.display === 'block') {
+        mobileMenu.style.display = 'none';
+    } else {
+        mobileMenu.style.display = 'block';
+    }
+}
